9f2725b63925addc9a07100803e6db25ee225bce
Viktor Senyk <victor.senyk.vs@gmail.com>
2024-08-19 14:25:04 +0300

    + improvements

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,10 +1,10 @@
-import users from '../../../data/users';
+import users from "../../../data/users";
 
 export default (req, res) => {
   const { page = 1, rowsPerPage = 16 } = req.query;
-  const startIndex = (parseInt(page) - 1) * parseInt(rowsPerPage);
+  const startIndex = (page - 1) * rowsPerPage;
+  const data = users.slice(startIndex, startIndex + parseInt(rowsPerPage));
+  const pagesCount = Math.ceil(users.length / rowsPerPage);
 
-  res
-    .status(200)
-    .json(users.slice(startIndex, startIndex + parseInt(rowsPerPage)));
+  res.status(200).json({ pagesCount, data });
 };
