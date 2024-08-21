85ee83e9ed7584fd10445b2a222debbd90bc9852
Viktor Senyk <victor.senyk.vs@gmail.com>
2024-08-21 13:44:57 +0300

    + `cors`

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,10 +1,13 @@
 import users from "../../../data/users";
+import { handleCors } from "../../../utils/cors";
 
 export default (req, res) => {
+  if (handleCors(req, res)) return;
+
   const { page = 1, rowsPerPage = 16 } = req.query;
   const startIndex = (page - 1) * rowsPerPage;
   const data = users.slice(startIndex, startIndex + parseInt(rowsPerPage));
   const pagesCount = Math.ceil(users.length / rowsPerPage);
 
-  res.status(200).json({ pagesCount, data });
+  return res.status(200).json({ pagesCount, data });
 };
--- pages/api/users/statistics/index.js
+++ pages/api/users/statistics/index.js
@@ -1,16 +1,18 @@
 import usersStatistic from "../../../../data/users/statistic";
+import { handleCors } from "../../../../utils/cors";
 
 export default (req, res) => {
-  const { userIds } = req.query;
+  if (handleCors(req, res)) return;
 
+  const { userIds } = req.query;
   if (userIds) {
     const idsArray = userIds.split(",").map((id) => parseInt(id));
     const filteredStats = usersStatistic
       .filter((stat) => idsArray.includes(stat.user_id))
       .sort((a, b) => a.user_id - b.user_id);
 
-    res.status(200).json(filteredStats);
-  } else {
-    res.status(400).json({ error: "No userIds provided" });
+    return res.status(200).json(filteredStats);
   }
+
+  return res.status(400).json({ error: "No userIds provided" });
 };
